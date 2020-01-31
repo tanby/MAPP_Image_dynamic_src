1bd595624ebed3d76e263280602b856a1a265ee3
Boon Yuen Tan <tan_boon_yuen@ichat.sp.edu.sg>
2020-01-31 17:18:18 +0800

    button click change image

--- app/src/main/java/edu/sp/p0123456/mapp_image_dynamic_src/MainActivity.java
+++ app/src/main/java/edu/sp/p0123456/mapp_image_dynamic_src/MainActivity.java
@@ -3,12 +3,36 @@ package edu.sp.p0123456.mapp_image_dynamic_src;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+
+    // for toggle purpose
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the imageview to change src later
+        iv = (ImageView) findViewById(R.id.myiv);
+    }
+
+    public void changeImg(View view) {
+
+        if(i==0) {
+            // change image src
+            iv.setImageResource(R.drawable.daaa);
+            i = 1;
+        }
+        else{
+            // change image src
+            iv.setImageResource(R.drawable.sp2);
+            i = 0;
+        }
     }
 }
