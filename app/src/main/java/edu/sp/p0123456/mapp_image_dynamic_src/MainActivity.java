ffd2e45f6cd63b3c16a83eee1f0915cd3f7871d0
Boon Yuen Tan <tan_boon_yuen@ichat.sp.edu.sg>
2020-01-31 16:48:50 +0800

    Initial commit

---
+++ app/src/main/java/edu/sp/p0123456/mapp_image_dynamic_src/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.sp.p0123456.mapp_image_dynamic_src;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
